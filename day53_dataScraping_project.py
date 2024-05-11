1828c88798310ca5230861381c4b10670fe4c912
Dhanush S Gowda <141866615+Dhanush-S-Gowda@users.noreply.github.com>
2024-05-12 01:09:18 +0530

    Update day53_dataScraping_project.py

--- day53_dataScraping_project.py
+++ day53_dataScraping_project.py
@@ -10,7 +10,7 @@ FORM_LINK = 'Enter your google form link'
 
 #Extracting data from the website using BeautifulSop 
 headers = {
-    'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
+    'User-Agent': "Enter your values",
     'Accept-Language': "en-US,en;q=0.9"
 }
 
